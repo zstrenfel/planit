ed6d8c8cfce7280c3df5c0313231cb118484e757
Sean <mdnsean@gmail.com>
2015-11-04 19:37:20 -0800

    skeleton for search tests

---
+++ spec/javascripts/searchSpec.js
@@ -0,0 +1,19 @@
+xdescribe("Search for destinations", function() {
+  
+  describe("Submit search", function() {
+    
+    it("calls findPlaces", function() {
+
+    });
+
+    it("lists relevant search results") {
+
+    });
+
+    it("adds a destination after I click +") {
+
+    });
+
+  });
+
+});
