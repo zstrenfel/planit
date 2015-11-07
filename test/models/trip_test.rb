016b32478a0a200722022428e6e71f921afaa913
kellykwak <kwakk6@gmail.com>
2015-11-07 04:49:16 +0000

    trip tests

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -18,4 +18,8 @@ class TripTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+    test "should not save trip without name" do
+    trip = Trip.new
+    assert_not trip.save, "Saved the trip without a name"
+  end
 end
