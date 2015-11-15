5930be38d8849d1abb1aa3c5146e78c22ef0530b
Sean <mdnsean@gmail.com>
2015-11-14 21:23:04 -0800

    new trip test passes. skeletons for other trip tests included

--- spec/features/trip_features_spec.rb
+++ spec/features/trip_features_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-feature "Add" do
+describe "New Trip" do
   before(:each) do
+    # register
     visit '/'
     fill_in 'reg-name', :with => 'Capy'
     fill_in 'reg-email', :with => 'capy@example.com'
@@ -9,6 +10,23 @@ feature "Add" do
     fill_in 'reg-pw-conf', :with => 'password'
     click_button 'Sign up'
     page.should have_content 'Logout'
+
+    # create a trip
+    click_link 'Trips'
+    find('.create-trip').click
+    fill_in 'location', with: 'Berkeley'
+    fill_in 'name', with: 'Cal'
+    fill_in 'start-date', with: '20-11-2015'
+    fill_in 'end-date', with: '25-11-2015'
+    click_link 'submit-trip'
+  end
+  
+  it "checks that trip info appears at the top" do
+  end
+
+  it "displays a functioning Google map"
+
+  it "invites friends" do
   end
 
   it "manually adds a destination" do
@@ -24,4 +42,14 @@ feature "Add" do
     page.should have_css('td', :text => 'Dest2')
     page.should have_css('td', :text => '456 Street Ave')
   end
+
+  describe "Destination search" do
+    it "displays search results" do
+    end
+
+    it "adds a destination"
+
+  end
+
+
 end
