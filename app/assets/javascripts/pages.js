c13a1d4e53b295d4693f03a015110a23c0fbd5cc
Zach Strenfel <zwstrenfel@gmail.com>
2015-11-20 16:38:57 -0800

    front page css changes

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,20 +1,26 @@
-$(window).load(function() {
-	(function() {
-		console.log('here');
-		var $target = $('.login-register');
+Pages = (function() {
+
+	var logInRegisterHandler = function() {
 		$('.log-in').on('click', function(e) {
 			console.log('click');
 			e.preventDefault();
 			$('.register-container').addClass('hidden');
 			$('.login-container').removeClass('hidden');
+		});
 
-		})
-		$('.regester').on('click', function(e) {
+		$('.register').on('click', function(e) {
 			console.log('click');
 			e.preventDefault();
 			$('.register-container').removeClass('hidden');
 			$('.login-container').addClass('hidden');
-		})
-	})()
-});
+		});
+	};
 
+	var start = function() {
+		logInRegisterHandler();
+	};
+
+	return {
+		start: start
+	};
+})();
